c560f33bc38cfe3696de845e492ddab4a5b6ae9f
FranciscoPerez <francisco.e.perez@gmail.com>
2018-12-07 17:14:17 -0500

    complete hash set implementation

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -7,19 +7,34 @@ class HashSet
   end
 
   def insert(key)
+    unless self.include?(key)
+      self[key.hash] << key
+      self.count += 1
+    end
+
+    if self.count == num_buckets
+      resize!
+    end
   end
 
   def include?(key)
     self[key.hash].include?(key)
   end
-
+  
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      self.count -= 1
+    end
   end
-
+  
+  protected
+  
+  attr_accessor :store
+  attr_writer :count
+  
   private
-
-  attr_reader :store
-
+  
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
     store[num % num_buckets]
@@ -30,6 +45,16 @@ class HashSet
   end
 
   def resize!
+    new_store = HashSet.new(num_buckets * 2)
 
+    self.store.each do |bucket|
+      bucket.each do |ele|
+        new_store.insert(ele)
+        self.remove(ele)
+        self.count += 1
+      end
+    end
+
+    self.store = new_store.store
   end
 end
